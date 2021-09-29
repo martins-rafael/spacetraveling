4f47bb91279497b517108dfd6ad3f164f5ef31b0
Rafael Martins <rafaeldcmartins@gmail.com>
2021-09-29 14:25:45 -0300

    Add Static generation (SSG) to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,40 @@
 import { GetStaticProps } from 'next';
-
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+import HomeTemplate, {
+  Post,
+  PostPagination,
+  HomeProps,
+} from '../templates/Home';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return <HomeTemplate postsPagination={postsPagination} />;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    { pageSize: 3 }
+  );
+  const posts = postsResponse.results.map(
+    (post): Post => ({
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    })
+  );
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
   };
-}
 
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
-// }
-
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
-// };
+  return {
+    props: { postsPagination },
+  };
+};
